de51fd19364cfa1507153bf219de55acd25ec8e0
Kenzyyy29 <144502437+Kenzyyy29@users.noreply.github.com>
2024-12-18 04:09:50 +0700

    Add files via upload

---
+++ cafetaria/README.md
@@ -0,0 +1,18 @@
+# Website Table Order Cafe (Cafetaria)
+
+## Customer
+- Memilih makanan dan minuman.
+- Melakukan proses checkout produk.
+- Melakukan transaksi.
+- Menerima struk pembelian produk via Whatsapp API.
+- Menerima bukti pembayaran via Whatsapp API.
+
+## Kasir
+- Menerima orderan produk dari customer.
+- Mengatur menu yang tersedia dan terjual habis.
+
+## Barista
+- Struk produk akan otomatis tercetak pada mesin printer kasir.
+
+## Kitchen
+- Struk produk akan otomatis tercetak pada mesin printer kasir.
---
+++ cafetaria/index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cafetaria - Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        input[type="text"]{
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Header -->
+    <nav class="navbar navbar-dark sticky-top" style="background-color: #1e1e1e;">
+  <div class="container-lg mt-2">
+  <a class="mb-3"href="."><img src="Icon/cafetaria white.png" width="120" height="19" /></a>
+
+  </div>
+</nav>
+    <!-- End Header -->
+
+    <!-- Content -->
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-lg mt-3">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <b>ORDER HERE!</b>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title">MAU PESAN APA HARI INI?</h4>
+                        <div class="container-lg mt-5">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group-expand-lg w-75 p-3 position-relative start-50 top-25 translate-middle">
+                                        <input type="text" placeholder="Cari menu favoritmu disini..." class="form-control">
+                                    </div>
+                                    <div class="row row-cols-md-3 g-2 p-2">
+                                        <div class="card mr-2" style="width: 12rem;">
+                                            <img class="card-img-top" src="image/Es Kopi Susu.png" alt="Card image cap">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Es Kopi Susu</h5>
+                                                <p class="card-text">Rp. 24,xxx,-</p>
+                                                <a href="#" class="btn btn-primary">Masukkan</a>
+                                            </div>
+                                        </div>
+                                        <div class="card mr-3" style="width: 12rem;">
+                                            <img class="card-img-top" src="image/Es Kopi Susu.png" alt="Card image cap">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Es Kopi Aren</h5>
+                                                <p class="card-text">Rp. 24,xxx,-</p>
+                                                <a href="#" class="btn btn-primary">Masukkan</a>
+                                            </div>
+                                        </div>
+                                    
+                                    </div>
+                                    <nav aria-label="Page navigation example">
+                                        <ul class="pagination justify-content-center mt-4">
+                                            <li class="page-item">
+                                            <a class="page-link" href="#" aria-label="Previous">
+                                                <span aria-hidden="true">&laquo;</span>
+                                            </a>
+                                            </li>
+                                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                            <li class="page-item">
+                                            <a class="page-link" href="#" aria-label="Next">
+                                                <span aria-hidden="true">&raquo;</span>
+                                            </a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-body-secondary">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Content -->
+
+    <!-- Footer -->
+
+    <!-- End Footer -->
+    <div class="fixed-bottom text-center mb-4">
+    Copyright ©2024 CAFETARIA.TECH
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
